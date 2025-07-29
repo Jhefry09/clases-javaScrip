651094786b84dda8fe867179946e3efe10943580
Jhefry09 <jhefrycabanillas@gmail.com>
2025-07-29 16:04:01 -0500

    cree mi primer hola mundo en javaScrip

---
+++ basico/00-holaMundo.js
@@ -0,0 +1,8 @@
+//mi primer hola mundo
+console.log("hola javaScrip");
+console.log(10 + 5);
+console.log(10 - 5);
+console.log(10 * 5);
+console.log(10 / 5);
+console.log(10 % 5);
+console.log(10 ** 5);
