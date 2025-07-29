ffa5b410d7d23d46c08aa700808eea4c3774e1d3
Jhefry09 <jhefrycabanillas@gmail.com>
2025-07-29 17:16:03 -0500

    aqui vi como declarar variables y los tipos de variables

---
+++ basico/01-variables.js
@@ -0,0 +1,12 @@
+//palabras reservadas para variables
+//var -> en desuso, declara la variable de forma global como si siempre estuviera en la cabezera del programa.
+var holaMundo = ("hola javaScrip con var, este es el segundo archivo de la clase");
+console.log(holaMundo);
+holaMundo = ("hola")
+console.log(holaMundo);
+// let -> el mas usado, sirve para declarar de forma local siguiendo el orden tipico de los demas lenguajes.
+let holaMundo2 = ("hola javaScrip con let");
+console.log(holaMundo)
+//const -> sirve para crear constantes que solo pueden cambiar en casos especiales
+const hola = "hola javaScrip con const"
+console.log(hola)
