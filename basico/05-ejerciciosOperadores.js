8bdb5269d4ed97064430769a8c747965481cc6de
Jhefry09 <jhefrycabanillas@gmail.com>
2025-08-01 22:41:44 -0500

    resolvi la parte de la creacion de las variables aritméticas

--- basico/05-ejerciciosOperadores.js
+++ basico/05-ejerciciosOperadores.js
@@ -1,4 +1,40 @@
 //1. Crea una variable para cada operacion aritmética
+console.log(" : ");
+console.log(7+8);
+console.log(10-8);
+console.log(5*5);
+console.log(25/5);
+console.log(19%5);
+console.log(5**5);
+let a = 5
+while (a>=1) {
+    a--
+    console.log(a);
+}
+while (a<=10) {
+    a++
+    console.log(a);
+}
+
+let b = 10
+
+b += 5
+console.log(b);
+
+b -=5
+console.log(b);
+
+b *=5
+console.log(b);
+
+b /=5
+console.log(b);
+
+b %=3
+console.log(b);
+
+b **=3
+console.log(b);
 
 //2. Crea una variable para cada tipo de operación de asignación , que haga uso de las variables utilizadas para las operaciones aritméticas
 
